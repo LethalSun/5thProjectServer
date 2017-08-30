ee905347491d57048d9137c44da0a250cdb592ba
DESKTOP-3VOQMPI\LongShore <massivedynamicplay@gmail.com>
2017-08-30 23:32:59 +0900

    몽고디비쪽 구현중

--- Server/DBServerASPdotNet/DBServer/DB/MongoDBLib.cs
+++ Server/DBServerASPdotNet/DBServer/DB/MongoDBLib.cs
@@ -7,5 +7,19 @@ namespace DBServer.DB
 {
     public class MongoDBLib
     {
+
+        public class DBUser
+        {
+            public string _id;
+            public Int64 UID;
+            public string PW;
+        }
+
+        public class DBBasicGameData
+        {
+            S
+        }
+            
+
     }
 }
