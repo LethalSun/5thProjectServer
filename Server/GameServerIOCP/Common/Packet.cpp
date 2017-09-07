2fc1e0c4ae4e6756ac4036c37700d38f232ba2b9
DESKTOP-3VOQMPI\LongShore <massivedynamicplay@gmail.com>
2017-09-07 10:15:39 +0900

    no message

---
+++ Server/GameServerIOCP/Common/Packet.cpp
@@ -0,0 +1,257 @@
+#include "Packet.h"
+
+namespace Packet
+{
+	bool CJsonSerializer::Serialize( IJsonSerializable* pObj, std::string& output )
+	{
+		if (pObj == NULL)
+			return false;
+
+		Json::Value serializeRoot;
+		pObj->Serialize(serializeRoot);
+
+		Json::StyledWriter writer;
+		output = writer.write( serializeRoot );
+
+		return true;
+	}
+
+	bool CJsonSerializer::Deserialize( IJsonSerializable* pObj, std::string& input )
+	{
+		if (pObj == NULL)
+			return false;
+
+		Json::Value deserializeRoot;
+		Json::Reader reader;
+
+		if ( !reader.parse(input, deserializeRoot) )
+			return false;
+
+		pObj->Deserialize(deserializeRoot);
+
+		return true;
+	}
+
+	void GAMESEVER_REQ_GAMESERVER_ENTER::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+		root["GameServerID"] = GameServerID;
+	}
+
+	void GAMESEVER_REQ_GAMESERVER_ENTER::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+		GameServerID = root.get("GameServerID", 0).asInt();
+	}
+
+	void GAMESEVER_RES_GAMESERVER_ENTER::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMESEVER_RES_GAMESERVER_ENTER::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMESEVER_NTF_NEW_USER::Serialize( Json::Value& root )
+	{
+		root["NewUserID"] = NewUserID;
+		root["NewUserIndex"] = NewUserIndex;
+	}
+
+	void GAMESEVER_NTF_NEW_USER::Deserialize( Json::Value& root )
+	{
+		NewUserID = root.get("NewUserID", "").asString();
+		NewUserIndex = root.get("NewUserIndex", "").asString();
+	}
+
+	void GAMESEVER_REQ_GAMESERVER_INFO::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+	}
+
+	void GAMESEVER_REQ_GAMESERVER_INFO::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+	}
+
+	void GAMESEVER_RES_GAMESERVER_INFO::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+		root["ServerUserNum"] = ServerUserNum;
+		root["ServerMatchNum"] = ServerMatchNum;
+	}
+
+	void GAMESEVER_RES_GAMESERVER_INFO::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+		ServerUserNum = root.get("ServerUserNum", 0).asInt();
+		ServerMatchNum = root.get("ServerMatchNum", 0).asInt();
+	}
+
+	void GAMESEVER_REQ_SHIP_DEPLOY_INFO::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+		root["NumOfTile"] = NumOfTile;
+		root["ShipOnTileInfo"] = ShipOnTileInfo;
+	}
+
+	void GAMESEVER_REQ_SHIP_DEPLOY_INFO::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+		NumOfTile = root.get("NumOfTile", 0).asInt();
+		ShipOnTileInfo = root.get("ShipOnTileInfo", 0).asInt();
+	}
+
+	void GAMESEVER_RES_SHIP_DEPLOY_INFO::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMESEVER_RES_SHIP_DEPLOY_INFO::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMESEVER_REQ_GAME_READY::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+	}
+
+	void GAMESEVER_REQ_GAME_READY::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+	}
+
+	void GAMESEVER_RES_GAME_READY::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMESEVER_RES_GAME_READY::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMESEVER_NTF_GAME_START::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMESEVER_NTF_GAME_START::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMESEVER_REQ_BOMB::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+		root["BombedTile"] = BombedTile;
+	}
+
+	void GAMESEVER_REQ_BOMB::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+		BombedTile = root.get("BombedTile", 0).asInt();
+	}
+
+	void GAMESEVER_RES_BOMB::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+		root["BombedTile"] = BombedTile;
+	}
+
+	void GAMESEVER_RES_BOMB::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+		BombedTile = root.get("BombedTile", 0).asInt();
+	}
+
+	void GAMESEVER_NTF_BOMB::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+		root["BombedTile"] = BombedTile;
+	}
+
+	void GAMESEVER_NTF_BOMB::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+		BombedTile = root.get("BombedTile", 0).asInt();
+	}
+
+	void GAMESEVER_NTF_GAMEND::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMESEVER_NTF_GAMEND::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMSERVER_REQ_USER_HEARTBEAT::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+	}
+
+	void GAMSERVER_REQ_USER_HEARTBEAT::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+	}
+
+	void GAMSERVER_RES_USER_HEARTBEAT::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMSERVER_RES_USER_HEARTBEAT::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMSERVER_REQ_USER_LOGOUT::Serialize( Json::Value& root )
+	{
+		root["ID"] = ID;
+		root["AuthToken"] = AuthToken;
+	}
+
+	void GAMSERVER_REQ_USER_LOGOUT::Deserialize( Json::Value& root )
+	{
+		ID = root.get("ID", "").asString();
+		AuthToken = root.get("AuthToken", "").asString();
+	}
+
+	void GAMSERVER_RES_USER_LOGIN::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMSERVER_RES_USER_LOGIN::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+	void GAMSERVER_NTF_USER_LOGOUT::Serialize( Json::Value& root )
+	{
+		root["Result"] = Result;
+	}
+
+	void GAMSERVER_NTF_USER_LOGOUT::Deserialize( Json::Value& root )
+	{
+		Result = root.get("Result", 0).asInt();
+	}
+
+}
---
+++ Server/GameServerIOCP/Common/Packet.h
@@ -0,0 +1,294 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "..\json\json\json.h"
+
+namespace Packet
+{
+	class IJsonSerializable
+	{
+	public:
+		virtual ~IJsonSerializable( void ) {};
+		virtual void Serialize( Json::Value& root ) =0;
+		virtual void Deserialize( Json::Value& root ) =0;
+	};
+
+	class CJsonSerializer
+	{
+	public:
+		static bool Serialize( IJsonSerializable* pObj, std::string& output );
+		static bool Deserialize( IJsonSerializable* pObj, std::string& input );
+
+	private:
+		CJsonSerializer( void ) {};
+	};
+
+	// C++ IOCP서버와 Unity C#이 통신하기 위한 패킷 정의
+	//게임서버 접속요청
+	class GAMESEVER_REQ_GAMESERVER_ENTER : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_REQ_GAMESERVER_ENTER( void ) {}
+		virtual ~GAMESEVER_REQ_GAMESERVER_ENTER( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+		int							GameServerID;
+	};
+
+	//게임서버 접속답변
+	class GAMESEVER_RES_GAMESERVER_ENTER : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_RES_GAMESERVER_ENTER( void ) {}
+		virtual ~GAMESEVER_RES_GAMESERVER_ENTER( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 접속 통지
+	class GAMESEVER_NTF_NEW_USER : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_NTF_NEW_USER( void ) {}
+		virtual ~GAMESEVER_NTF_NEW_USER( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					NewUserID;
+		std::string					NewUserIndex;
+	};
+
+	//게임서버 정보 요청
+	class GAMESEVER_REQ_GAMESERVER_INFO : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_REQ_GAMESERVER_INFO( void ) {}
+		virtual ~GAMESEVER_REQ_GAMESERVER_INFO( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+	};
+
+	//게임서버 정보 답변
+	class GAMESEVER_RES_GAMESERVER_INFO : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_RES_GAMESERVER_INFO( void ) {}
+		virtual ~GAMESEVER_RES_GAMESERVER_INFO( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+		int							ServerUserNum;
+		int							ServerMatchNum;
+	};
+
+	//게임서버 배 배치 정보 입력 요청
+	class GAMESEVER_REQ_SHIP_DEPLOY_INFO : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_REQ_SHIP_DEPLOY_INFO( void ) {}
+		virtual ~GAMESEVER_REQ_SHIP_DEPLOY_INFO( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+		unsigned					NumOfTile;
+		std::vector<int>			ShipOnTileInfo;
+	};
+
+	//게임서버 배 배치 정보 입력 답변
+	class GAMESEVER_RES_SHIP_DEPLOY_INFO : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_RES_SHIP_DEPLOY_INFO( void ) {}
+		virtual ~GAMESEVER_RES_SHIP_DEPLOY_INFO( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 게임시작 요청
+	class GAMESEVER_REQ_GAME_READY : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_REQ_GAME_READY( void ) {}
+		virtual ~GAMESEVER_REQ_GAME_READY( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+	};
+
+	//게임서버 게임시작 답변
+	class GAMESEVER_RES_GAME_READY : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_RES_GAME_READY( void ) {}
+		virtual ~GAMESEVER_RES_GAME_READY( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 게임시작 당사자들에게 통지 누구의 턴인지 리절트로 알수 있다.
+	class GAMESEVER_NTF_GAME_START : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_NTF_GAME_START( void ) {}
+		virtual ~GAMESEVER_NTF_GAME_START( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 폭탄 배치 요청
+	class GAMESEVER_REQ_BOMB : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_REQ_BOMB( void ) {}
+		virtual ~GAMESEVER_REQ_BOMB( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+		int							BombedTile;
+	};
+
+	//게임서버 폭탄 배치 답변
+	class GAMESEVER_RES_BOMB : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_RES_BOMB( void ) {}
+		virtual ~GAMESEVER_RES_BOMB( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+		int							BombedTile;
+	};
+
+	//게임서버 폭탄 배치 통지 Result로 맞았는지 아닌지 그리고 턴을 알수 있다.
+	class GAMESEVER_NTF_BOMB : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_NTF_BOMB( void ) {}
+		virtual ~GAMESEVER_NTF_BOMB( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+		int							BombedTile;
+	};
+
+	//게임서버 종료 통지
+	class GAMESEVER_NTF_GAMEND : public IJsonSerializable
+	{
+	public:
+		GAMESEVER_NTF_GAMEND( void ) {}
+		virtual ~GAMESEVER_NTF_GAMEND( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 유저 하트비트 요청
+	class GAMSERVER_REQ_USER_HEARTBEAT : public IJsonSerializable
+	{
+	public:
+		GAMSERVER_REQ_USER_HEARTBEAT( void ) {}
+		virtual ~GAMSERVER_REQ_USER_HEARTBEAT( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+	};
+
+	//게임서버 유저 하트비트 답변
+	class GAMSERVER_RES_USER_HEARTBEAT : public IJsonSerializable
+	{
+	public:
+		GAMSERVER_RES_USER_HEARTBEAT( void ) {}
+		virtual ~GAMSERVER_RES_USER_HEARTBEAT( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 로그아웃 요청
+	class GAMSERVER_REQ_USER_LOGOUT : public IJsonSerializable
+	{
+	public:
+		GAMSERVER_REQ_USER_LOGOUT( void ) {}
+		virtual ~GAMSERVER_REQ_USER_LOGOUT( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		std::string					ID;
+		std::string					AuthToken;
+	};
+
+	//게임서버 로그아웃 답변 답변이 오면 끊는다.
+	class GAMSERVER_RES_USER_LOGIN : public IJsonSerializable
+	{
+	public:
+		GAMSERVER_RES_USER_LOGIN( void ) {}
+		virtual ~GAMSERVER_RES_USER_LOGIN( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	//게임서버 로그아웃 통지
+	class GAMSERVER_NTF_USER_LOGOUT : public IJsonSerializable
+	{
+	public:
+		GAMSERVER_NTF_USER_LOGOUT( void ) {}
+		virtual ~GAMSERVER_NTF_USER_LOGOUT( void ) {}
+		virtual void Serialize ( Json::Value& root );
+		virtual void Deserialize( Json::Value& root );
+
+		int							Result;
+	};
+
+	enum PacketId
+	{
+		ID_GAMESEVER_REQ_GAMESERVER_ENTER			= 101,
+		ID_GAMESEVER_RES_GAMESERVER_ENTER			= 102,
+		ID_GAMESEVER_NTF_NEW_USER			= 103,
+		ID_GAMESEVER_REQ_GAMESERVER_INFO			= 104,
+		ID_GAMESEVER_RES_GAMESERVER_INFO			= 105,
+		ID_GAMESEVER_REQ_SHIP_DEPLOY_INFO			= 106,
+		ID_GAMESEVER_RES_SHIP_DEPLOY_INFO			= 107,
+		ID_GAMESEVER_REQ_GAME_READY			= 108,
+		ID_GAMESEVER_RES_GAME_READY			= 109,
+		ID_GAMESEVER_NTF_GAME_START			= 110,
+		ID_GAMESEVER_REQ_BOMB			= 111,
+		ID_GAMESEVER_RES_BOMB			= 112,
+		ID_GAMESEVER_NTF_BOMB			= 113,
+		ID_GAMESEVER_NTF_GAMEND			= 114,
+		ID_GAMSERVER_REQ_USER_HEARTBEAT			= 115,
+		ID_GAMSERVER_RES_USER_HEARTBEAT			= 116,
+		ID_GAMSERVER_REQ_USER_LOGOUT			= 117,
+		ID_GAMSERVER_RES_USER_LOGIN			= 118,
+		ID_GAMSERVER_NTF_USER_LOGOUT			= 119,
+	};
+}
